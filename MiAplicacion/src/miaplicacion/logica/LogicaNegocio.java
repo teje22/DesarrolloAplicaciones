3a37954f58cd8aa1550a7256e7feba9c62886c2e
Pablo Tejedor Rivadulla <2021360@iesluisbraille.es>
2022-10-14 10:45:47 +0200

    Version 1.0

---
+++ MiAplicacion/src/miaplicacion/logica/LogicaNegocio.java
@@ -0,0 +1,26 @@
+package miaplicacion.logica;
+
+import java.util.List;
+import java.util.ArrayList;
+import miaplicacion.dto.Usuario;
+
+/**
+ *
+ * @author PabloTejedorRivadull
+ */
+public class LogicaNegocio {
+    
+    public List<Usuario> listaUsuarios = new ArrayList<>();
+    
+    public LogicaNegocio(){
+        listaUsuarios = new ArrayList<>();
+        listaUsuarios.add(new Usuario("Pablo","Alumno"));
+        listaUsuarios.add(new Usuario("David","Alumno"));
+        listaUsuarios.add(new Usuario("Alfonso","Profesor"));
+        listaUsuarios.add(new Usuario("Rocio","Profesor"));
+    }
+    
+    public List<Usuario> getListaUsuarios(){
+        return listaUsuarios;
+    }
+}
