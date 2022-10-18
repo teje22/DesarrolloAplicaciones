5a5e01aebe04ae384b15b5e14f3f5866b021eeb2
Pablo Tejedor Rivadulla <2021360@iesluisbraille.es>
2022-10-18 12:09:21 +0200

    Version 2.0

---
+++ AplicacionFCTs/src/dto/Usuario.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author PabloTejedorRivadull
+ */
+public class Usuario {
+    
+    private String nombre;
+    private String rol;
+
+    public Usuario(String nombre, String rol) {
+        this.nombre = nombre;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    
+    
+}
---
+++ AplicacionFCTs/src/gui/tablemodel/UsuariosTableModel.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui.tablemodel;
+
+import dto.Usuario;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author PabloTejedorRivadull
+ */
+public class UsuariosTableModel extends AbstractTableModel {
+    
+    private List<Usuario> listUsuario;
+    private String[] columnas = {"Nombre", "Rol"};
+    
+    public UsuariosTableModel(List<Usuario> listUsuario){
+        this.listUsuario = listUsuario;
+    }
+    
+    
+    @Override
+    public int getRowCount() 
+    {
+        return listUsuario.size();
+    }
+
+    @Override
+    public int getColumnCount() 
+    {
+        return columnas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) 
+    {
+        switch (columnIndex)
+        {
+            case 0: 
+                return listUsuario.get(rowIndex).getNombre();
+            case 1:
+                return listUsuario.get(rowIndex).getRol();
+        }
+        return null;
+    }
+
+    @Override
+    public String getColumnName(int column) 
+    {
+        return columnas[column];
+    }
+    
+    
+    
+}
